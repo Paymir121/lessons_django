5b6fb6c0d1ef343ce92c1da53442e7c15132016e
Nikki Nikkonor <110825664+Paymir121@users.noreply.github.com>
2025-06-30 14:25:04 +0400

    Инит ООП sql и индексы

---
+++ django_orm/api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import viewsets
+from .models import Author, Book, Review
+from .serializers import AuthorSerializer, BookSerializer, ReviewSerializer
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
