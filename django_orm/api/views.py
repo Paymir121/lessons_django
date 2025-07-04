d081b0aa7f7af7b4ae0e3be126dfd2e2ddcbc448
Nikki Nikkonor <110825664+Paymir121@users.noreply.github.com>
2025-07-04 23:11:10 +0400

    Закончил

--- django_orm/api/views.py
+++ django_orm/api/views.py
@@ -1,23 +1,11 @@
+from django.db.models import Q, F, Count, Avg, ExpressionWrapper
+from django.forms import DecimalField
 from rest_framework import viewsets
-from rest_framework import serializers
 
 from book.models import Author, Book, Review
 from book.add_init_data import load_data_from_json
+from api.serializers import AuthorSerializer, BookSerializer, ReviewSerializer
 
-class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = '__all__'
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = '__all__'
-
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = '__all__'
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
@@ -28,11 +16,16 @@ class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.filter(id__in=[1, 3, 5])#Найди авторов с идентификаторами 1, 3 и 5.
     queryset = Author.objects.filter(books__published_date__range=['2023-01-01', '2023-12-31'])# Найди книги, опубликованные с 1 января по 31 декабря 2023 года.
     queryset = Author.objects.filter(last_name__iregex=r'^Mc') # Найди авторов, у которых фамилия начинается на «Mc»
+    queryset = Author.objects.filter(Q(first_name='Alice') | ~Q(last_name='Brown')) # Найди книги, у которых цена равна скидке.Найди авторов с именем «Alice» или с фамилией, не равной «Brown».
+    queryset = Author.objects.annotate(total_books=Count('books')) # Подсчитай количество книг каждого автора.
+    queryset = Author.objects.prefetch_related('books').all() # Получи список авторов и всех их книг так, чтобы было выполнено ровно два SQL-запроса.
+
     def list(self, request, *args, **kwargs):
         # Запускаем загрузку данных перед выполнением стандартного list
         load_data_from_json()
 
         return super().list(request, *args, **kwargs)
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
@@ -54,9 +47,14 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.filter(published_date__week=1) # Найди книги, опубликованные на 23-й неделе года.
     queryset = Book.objects.filter(published_date__quarter=2)# Найди книги, опубликованные во втором квартале года.
     queryset = Book.objects.filter(published_date__date='2025-06-30') # Найди отзывы, сделанные в определённую дату.
+    queryset = Book.objects.filter(price=F('discount')) # Найди книги, у которых цена равна скидке.
+    queryset = Book.objects.filter(price__gt=F('discount')) # Найди книги, у которых цена больше скидки.
+    queryset = Book.objects.annotate(avg_rating=Avg('reviews__rating')) # Подсчитай средний рейтинг каждой книги.
+    queryset = Book.objects.annotate(final_price=ExpressionWrapper(F('price') - F('discount'), output_field=DecimalField())) # Посчитай окончательную цену книги (цена минус скидка).
+    queryset = Book.objects.select_related('author').all() # Получи список книг и авторов так, чтобы выполнить всего один SQL-запрос.
+
 
 class ReviewViewSet(viewsets.ModelViewSet):
-
     serializer_class = ReviewSerializer
     queryset = Review.objects.all()
     queryset = Review.objects.filter(comment__isnull=True) # Найди все отзывы без комментариев
